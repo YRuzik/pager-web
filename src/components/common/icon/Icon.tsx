7e31cc63205eb72f84fa6cd228f730e4636717a2
yruzik <yruzik@gmail.com>
2024-01-30 17:15:24 +0300

    icon component

---
+++ src/components/common/icon/Icon.tsx
@@ -0,0 +1,18 @@
+import {FC} from "react";
+
+export enum AppIcons {
+}
+
+type IconProps = {
+    size?: number;
+    onClick?: () => void;
+    icon: AppIcons;
+}
+
+const Icon: FC<IconProps> = ({icon, size = 36, onClick}) => {
+    return (
+        <div onClick={onClick} className={onClick ? "icon-button" : undefined}>
+            <img src={icon} alt={icon.toString()} width={size} height={size}/>
+        </div>
+    )
+}
